f2835c05f6f2e774614f865cb29526fbae41045c
roxzlir <emil.ch.nordin@gmail.com>
2024-05-22 13:35:44 +0200

    Zoran - Emil : Updated Model for campaign dates

--- DTO/ProductDTO.cs
+++ DTO/ProductDTO.cs
@@ -5,17 +5,15 @@ namespace ByteBazaarAPI.DTO
     public class ProductDTO
     {
         public string Title { get; set; }
-
         public string Description { get; set; }
-
         public decimal Price { get; set; }
-
         public int FkCategoryId { get; set; }
-
         public int Quantity { get; set; }
         public string ImageURL { get; set; }
         public bool IsCampaign { get; set; } = false;
         public decimal CampaignPercent { get; set; } = 1;
         public decimal TempPrice { get; set; } = 0;
+        public DateTime? CampaignStart { get; set; }
+        public DateTime? CampaignEnd { get; set; }
     }
 }
